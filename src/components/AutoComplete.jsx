84cfdeef641e12e88ea86de8c0fb166cd668dc83
AndreMuderhwa <safariandre66@gmail.com>
2024-04-30 11:53:12 +0200

    adding search component

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,7 +1,48 @@
+import { useState,useEffect } from "react";
+import finnHub from "../APIs/finnHub";
+
 export const AutoComplete=()=>{
+    const [search,setSearch]=useState("");
+    const [result,setResult]=useState([]);
+    useEffect(()=>{
+        let isMounted=true
+        const fetchData=async()=>{
+            try{
+                const response =await finnHub.get("/search",{
+                    params:{
+                        q:search
+                    }
+                })
+
+            console.log(response)
+            if(isMounted){
+              setResult(response.data.result)  
+            }
+            
+            }
+            
+            catch(err){
+                console.log(err)
+            }
+        }
+        if(search.length > 0){
+          fetchData()  
+        }
+        return ()=>(isMounted=false)
+        
+    },[search])
     return (
-        <div>
-            Auto Complete 
+        <div className="w-50 p-5 rounded mx-auto">
+            <div className="form-floating dropdown">
+                <input style={{ backgroundColor:"rgba(145,178,171,0.04)"}} type="text" id="search" className="form-control" value={search} placeholder="Search" autoComplete="off" onChange={(e)=>setSearch(e.target.value)}/>
+                <label htmlFor="search">Search</label>
+                <ul className="dropdown-menu">
+                    <li>Stock 1</li>
+                    <li>Stock 2</li>
+                    <li>Stock 3</li>
+                </ul>
+
+            </div>
         </div>
-    )
+    ) 
 }
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,8 +1,19 @@
 import { useState,useEffect } from "react";
 import finnHub from "../APIs/finnHub";
+import {BsFillCaretDownFill} from 'react-icons/bs';
+import {BsFillCaretUpFill} from 'react-icons/bs';
+
+
 export const StockList=()=>{
-    const [stock,setStock]=useState();
+    const [stock,setStock]=useState([]);
     const [watchList, setWatchList]=useState(["GOOGL","MSFT","AMZN"]);
+
+    const changeColor =(change)=>{
+        return change>0 ? "success" : "danger"
+    }
+    const renderIcon=(data)=>{
+        return data >0 ? <BsFillCaretUpFill/> : <BsFillCaretDownFill/>
+    }
 
     useEffect(()=>{
         let isMounted=true
@@ -40,7 +51,39 @@ export const StockList=()=>{
     },[])
     return (
         <div>
-            Stock List
+            <table className="table hover mt-5">
+                <thead style={{color:"rgb(79,89,102)"}}>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Chg</th>
+                        <th scope="col">Chg%</th>
+                        <th scope="col">High</th>
+                        <th scope="col">Low</th>
+                        <th scope="col">Open</th>
+                        <th scope="col">Pclose</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {stock.map((stockData)=>{
+                        return(
+                            <tr className="table-row" key={stockData.symbol}>
+                                <th scope="row">{stockData.symbol}</th>
+                                <td>{stockData.data.c}</td>
+                                <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d} {renderIcon(stockData.data.d)}</td>
+                                <td className={`text-${changeColor(stockData.data.dp)}`}>{stockData.data.dp} {renderIcon(stockData.data.dp)}</td>
+                                <td>{stockData.data.h}</td>
+                                <td>{stockData.data.l}</td>
+                                <td>{stockData.data.o}</td>
+                                <td>{stockData.data.pc}</td>
+
+
+                            </tr>
+                        )
+                    })}
+                </tbody>
+
+            </table>
         </div>
     )
 }
